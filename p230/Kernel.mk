5d474d07daa569b8a982801ddecdc1ac1acfda27
Nanxin Qin <nanxin.qin@amlogic.com>
2017-09-04 20:46:23 +0800

    common: modify modules dir in the sysfs to compatible with kernel 3.14 [2/3]
    PD#150498: modify modules dir in the sysfs to compatible with kernel 3.14
    
    Change-Id: Ifbb93b62fdda5108ce250ddebcf9ab89ca7ddcfc
    Signed-off-by: Nanxin Qin <nanxin.qin@amlogic.com>

--- p230/Kernel.mk
+++ p230/Kernel.mk
@@ -30,28 +30,29 @@ INTERMEDIATES_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image.gz
 TARGET_AMLOGIC_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/uImage
 TARGET_AMLOGIC_INT_RECOVERY_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image_recovery
 
-BOARD_VENDOR_KERNEL_MODULES := $(PRODUCT_OUT)/obj/lib_vendor/audio_data.ko \
+BOARD_VENDOR_KERNEL_MODULES := \
+	$(PRODUCT_OUT)/obj/lib_vendor/audio_data.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/dhd.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/media_clock.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vavs.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vh264_multi.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vmjpeg.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vmpeg4.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vvc1.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/ddr_window_64.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/encoder.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vh264_4k2k.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vh264_mvc.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vmjpeg_multi.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vmpeg4_multi.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vvp9.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/decoder_common.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/media_clock.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/firmware.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/decoder_common.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/stream_input.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vh264.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vh265.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vmpeg12.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/vreal.ko
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_avs.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mpeg12.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mpeg4.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mmpeg4.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_h264.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mh264.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_h264mvc.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_h264_4k2k.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_h265.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mjpeg.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mmjpeg.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_vc1.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_vp9.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_real.ko \
+	$(PRODUCT_OUT)/obj/lib_vendor/encoder.ko
 
 WIFI_OUT  := $(TARGET_OUT_INTERMEDIATES)/hardware/wifi
 
