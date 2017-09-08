1826574311f5422eb9239313d77f68f03b3c53dd
Nanxin Qin <nanxin.qin@amlogic.com>
2017-09-08 18:06:37 +0800

    common: add the tee moudule. [3/3]
    PD#150739: add tee moudule.
    
    Change-Id: If214d4296ddee687a8301892138d7af49ca3b5e1
    Signed-off-by: Nanxin Qin <nanxin.qin@amlogic.com>

--- p230/Kernel.mk
+++ p230/Kernel.mk
@@ -33,26 +33,9 @@ TARGET_AMLOGIC_INT_RECOVERY_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Ima
 BOARD_VENDOR_KERNEL_MODULES := \
 	$(PRODUCT_OUT)/obj/lib_vendor/audio_data.ko \
 	$(PRODUCT_OUT)/obj/lib_vendor/dhd.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/ddr_window_64.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/media_clock.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/firmware.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/decoder_common.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/stream_input.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_avs.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mpeg12.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mpeg4.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mmpeg4.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_h264.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mh264.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_h264mvc.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_h264_4k2k.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_h265.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mjpeg.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_mmjpeg.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_vc1.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_vp9.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/amvdec_real.ko \
-	$(PRODUCT_OUT)/obj/lib_vendor/encoder.ko
+	$(PRODUCT_OUT)/obj/lib_vendor/ddr_window_64.ko
+
+BOARD_VENDOR_KERNEL_MODULES += $(DEFAULT_MEDIA_KERNEL_MODULES)
 
 WIFI_OUT  := $(TARGET_OUT_INTERMEDIATES)/hardware/wifi
 
